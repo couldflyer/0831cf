f7190cb37e7e8fcbc99994d2456256a7d15565ae
couldflyer <138887155+couldflyer@users.noreply.github.com>
2024-08-28 18:28:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'ed5120ce-0193-4fc7-830e-127669ea5732';
 
-const พร็อกซีไอพีs = ['154.17.10.75'];
+const พร็อกซีไอพีs = ['workers.cloudflare.cyou'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
